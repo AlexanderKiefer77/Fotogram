ff5795fb5b8f9073ae8f4d63e7e13b128c198d20
Alexander Kiefer <Alexander.Kiefer77@protonmail.com>
2025-06-21 15:21:23 +0200

    stand for modified html

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-let picturesArray = [
+let pictures = [
     "pictureOne.jpg",
     "pictureTwo.jpg",
     "pictureThree.jpg",
@@ -18,37 +18,38 @@ let currentPictures = []; // muss definiert sein, sonst wird es in der function 
 
 function render() {
     let pictureRef = document.getElementById('pictureField');
+    console.log(pictureRef);
+    
     pictureRef.innerHTML = ''; // leert den Inhalt des div´s
 
-    for (let index = 0; index < picturesArray.length; index++) {
+    for (let index = 0; index < pictures.length; index++) {
         pictureRef.innerHTML += pictureRendering(index);
-    }
-    
+    }    
 }
 
-
-function pictureRendering(index) { // erstellt die kleinen Bilder
-    
+function pictureRendering(index) { // erstellt die kleinen Bilder    //
     return `<div>             
-                <img src="./assets/img/${picturesArray[index]}" alt="Bilder aus Bregenz" onclick="toggleOverlay(${index})" class="picture">                                               
+                <img src="./assets/img/${pictures[index]}" alt="Bilder aus Bregenz" onclick="openOverlay(${index})" class="picture">                                               
             </div> `
 }
 
-function toggleOverlay(index) { // erstellt das große Bild beim anklicken eines kleinen Bildes
-    let overlayRef = document.getElementById('overlay'); // Variable dafür erstellt
 
-    overlayRef.classList.toggle('d_none'); // class wird in div eingefügt
+
+function openOverlay(index) { // erstellt das große Bild beim anklicken eines kleinen Bildes
+    let overlayRef = document.getElementById('overlay'); // Variable dafür erstellt
+  
+    //overlayRef.classList.add ('d_none'); // class wird in div eingefügt
 
     overlayRef.innerHTML = `<div class="innerOverlay">
                                 <img src="./assets/back.png" alt="Pfeil nach Links" onclick="pictureBackwards()" class="arrow">
-                                <img src="./assets/img/${picturesArray[index]}" class="pictureInOverlay">
+                                <img src="./assets/img/${pictures[index]}" onclick="closePicture()"class="pictureInOverlay">
                                 <img src="./assets/forward.png" alt="Pfeil nach Links" onclick="pictureForwards(${[index]})" class="arrow">
                           </div>`
-
+console.log(overlayRef);
 }
 
 function pictureBackwards(index) {
-    console.log(picturesArray[index]);
+    console.log(pictures[index]);
     
 }
 
@@ -56,3 +57,14 @@ function pictureForwards(index) {
     console.log("Bild forwärts" + [index]);
     
 }
+
+function closePicture() {
+    console.log("Bild schliessen");
+    
+}
+
+// mit dieser function wird das eventbubbling verhindert auf der orangenene Fläche.
+        function logDownWBubblingPrevention(event) { // event wird übergeben
+            console.log("logDown");
+            event.stopPropagation(); // das ist eine function von diesem event
+        }
