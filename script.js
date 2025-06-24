cd7bdae8ee8b456a7489b7115bf392916ae98c6f
Alexander Kiefer <Alexander.Kiefer77@protonmail.com>
2025-06-24 09:36:52 +0200

    added slide function

--- script.js
+++ script.js
@@ -19,22 +19,21 @@ let commentsPictures = [
     "Ausblick vom Pfänder Richtung Schweiz",
     "Ausblick vom Pfänder zum Bodensee",
     "Ausblick vom Pfänder zum Bodensee",
-    "Ausblick vom Pfänder zum Bodensee Richtung Lindau",
-    "Ausblick vom Pfänder zum Bodensee Richtung Lindau",
+    "Ausblick vom Pfänder zum Bodensee",
+    "Ausblick vom Pfänder zum Bodensee",
     "Bregenz Hafen - MS Sonnenkönigin",
     "Bregenz Hafeneinfahrt",
     "Ausblick Hafen Richtung Lindau",
-    "Ausblick Hafen Richtung Bregenz Nordseite",
-    "Bregenz Hafen - Blick von Promenade zum Hafen",
+    "Ausblick Hafen Bregenz Nordseite",
+    "Bregenz Blick von Promenade zum Hafen",
     "Ausblick Hafen Richtung Bregenz Südseite"
 ]
 
-let currentPictures = []; // muss definiert sein, sonst wird es in der function zu Fehler führen
-let currentComments = [];
-
+let overlayRef = document.getElementById('overlay');
+let displayAOT = document.getElementById('aot');
 
 function render() { // wird mit body onload gestartet, fügt die Überschrift hinzu und erstellt 
-                    // die kleinen Fotos in Verbindung mit der Funktion "pictureRendering"
+    // die kleinen Fotos in Verbindung mit der Funktion "pictureRendering"
     let pictureRef = document.getElementById('pictureField');
     pictureRef.innerHTML = `<div class="headline">
                                 <h1>Meine Fotos aus Bregenz 2025</h1>
@@ -50,30 +49,30 @@ function pictureRendering(index) { // erstellt die kleinen Bilder    //
             </div>`
 }
 
-
-let overlayRef = document.getElementById('overlay');
-let displayAOT = document.getElementById('aot');
-
 function openOverlay(index) { // erstellt den Inhalt des Overlay Bildes
+    overlayPictureRendering(index);
+    toggleOverlay(); // startet die toggle function
+    displayAOT.classList.add("d_none"); // fügt dem AOT die class d_none hinzu, damit wird der Inhalt des AOT ausgeblendet
+}
+
+function overlayPictureRendering(index) {
     overlayRef.innerHTML = `<div class="innerOverlay">                               
-                                
+                                <figure>
                                     <img src="./assets/img/${pictures[index]}" alt="Foto ${commentsPictures[index]}" onclick="closeOverlay()" class="pictureInOverlay">
                                     <div class="navigate">
-                                        <img src="./assets/back.png" alt="Pfeil nach Links" onclick="pictureBackwards()" class="arrow">
-                                        <h2>${commentsPictures[index]}</h2>
-                                        <img src="./assets/forward.png" alt="Pfeil nach Links" onclick="pictureForwards(${[index]})" class="arrow">
+                                        <img src="./assets/back.png" alt="Pfeil Bild zurück" onclick="prevPicture(${index})" class="arrow">
+                                        <figcaption>
+                                            <h2>${commentsPictures[index]}</h2>
+                                        </figcaption>    
+                                        <img src="./assets/forward.png" alt="Pfeil Bild vorwärts" onclick="nextPicture(${index})" class="arrow">
                                     </div>
-                                                                                            
+                                </figure>                                                            
                             </div>`;
-    toggleOverlay(); // startet die toggle function
-    
-  displayAOT.classList.add("d_none"); // fügt dem AOT die class d_none hinzu, damit wird der Inhalt des AOT ausgeblendet
-}
+};
 
-function toggleOverlay() {   
-    overlayRef.classList.toggle('overlayClass');  
+function toggleOverlay() {
+    overlayRef.classList.toggle('overlayClass');
 }
-
 
 function closeOverlay() { // function für Overlay zu schliessen
     overlayRef.innerHTML = ''; // leert den Inhalt des Overlay
@@ -81,19 +80,12 @@ function closeOverlay() { // function für Overlay zu schliessen
     displayAOT.classList.remove("d_none"); // entfernt dem AOT wieder die class d_none, damit wird der Inhalt des AOT wieder eingeblendet
 }
 
-
-function pictureBackwards(index) {
-    console.log("Bild zurück");
+function prevPicture(index) {
+    index = index - 1;
+    overlayPictureRendering(index);
 }
 
-function pictureForwards(index) {
-    console.log("Bild forwärts" + [index]);
-}
-
-
-
-// mit dieser function wird das eventbubbling verhindert auf der orangenene Fläche.
-function logDownWBubblingPrevention(event) { // event wird übergeben
-    console.log("logDown");
-    event.stopPropagation(); // das ist eine function von diesem event
+function nextPicture(index) {
+    index = index + 1;
+    overlayPictureRendering(index);
 }
